43390e8c36d45672efe7353c2d1695d58f13c7fe
David Salazar <davis.dassr@gmail.com>
2019-04-15 16:20:12 -0500

    Primer Programa Python

---
+++ hola.py
@@ -0,0 +1,8 @@
+"""
+    Este es mi primer ejemplo
+    en lenguaje Python
+    Nombre: David Alexander
+    Apellido: Salazar Solorzano
+"""    
+print("Hola mundo UTPL")
+
---
+++ presentacion.py
@@ -0,0 +1,9 @@
+"""
+    Este es mi primer ejemplo
+    en lenguaje Python
+    Nombre: David Alexander
+    Apellido: Salazar Solorzano
+"""    
+print("Nombre: David Alexander") 
+print("Apellido: Salazar Solorzano")
+
