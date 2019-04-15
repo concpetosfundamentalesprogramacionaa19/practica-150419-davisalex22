50f9af816418eb0e41077222b2f0a56d892e3d0f
David Salazar <davis.dassr@gmail.com>
2019-04-15 16:46:31 -0500

    Presentacion 2 Python

---
+++ presentacion2.py
@@ -0,0 +1,12 @@
+"""
+     Este es mi tercer ejemplo
+     en lenguaje Python
+
+     Ingreso de informacion por teclado
+
+"""
+nombre = input("Por favor ingrese su nombre:\n")
+apellido = input("Por favor ingrese su apellido:\n")
+#print("%s-%s"%(nombre,apellido))
+#print(nombre,apellido)
+print("Mi nombre es: %s\n\t Mi apellido es: \n%s" % (nombre,apellido))
